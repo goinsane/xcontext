ef4681727014b4f5df4885e3695db997e53f8729
Orkun Karaduman <orkunkaraduman@gmail.com>
2020-12-30 16:53:43 +0300

    improved DelayContext tests

--- xcontext_test.go
+++ xcontext_test.go
@@ -7,33 +7,48 @@ import (
 )
 
 func TestDelayContext(t *testing.T) {
-	var timeout, delay, period time.Duration
+	var timeout, delay, cancel, period time.Duration
 
-	timeout, delay = 1*time.Second, 2*time.Second
-	t.Logf("test with timeout=%v delay=%v", timeout, delay)
-	period = testDelayContext(timeout, delay)
+	timeout, delay, cancel = 1*time.Second, 2*time.Second, 0
+	t.Logf("test with timeout=%v delay=%v cancel=%v", timeout, delay, cancel)
+	period = testDelayContext(timeout, delay, cancel)
 	if period < delay {
-		t.Errorf("test failed with timeout=%v delay=%v period=%v", timeout, delay, period)
+		t.Errorf("test failed with timeout=%v delay=%v cancel=%v period=%v", timeout, delay, cancel, period)
 	} else {
-		t.Logf("test ok with timeout=%v delay=%v period=%v", timeout, delay, period)
+		t.Logf("test ok with timeout=%v delay=%v cancel=%v period=%v", timeout, delay, cancel, period)
 	}
 
-	timeout, delay = 2*time.Second, 1*time.Second
-	t.Logf("test with timeout=%v delay=%v", timeout, delay)
-	period = testDelayContext(timeout, delay)
+	timeout, delay, cancel = 2*time.Second, 1*time.Second, 0
+	t.Logf("test with timeout=%v delay=%v cancel=%v", timeout, delay, cancel)
+	period = testDelayContext(timeout, delay, cancel)
 	if period < timeout {
-		t.Errorf("test failed with timeout=%v delay=%v period=%v", timeout, delay, period)
+		t.Errorf("test failed with timeout=%v delay=%v cancel=%v period=%v", timeout, delay, cancel, period)
 	} else {
-		t.Logf("test ok with timeout=%v delay=%v period=%v", timeout, delay, period)
+		t.Logf("test ok with timeout=%v delay=%v cancel=%v period=%v", timeout, delay, cancel, period)
+	}
+
+	timeout, delay, cancel = 2*time.Second, 2*time.Second, 1*time.Second
+	t.Logf("test with timeout=%v delay=%v cancel=%v", timeout, delay, cancel)
+	period = testDelayContext(timeout, delay, cancel)
+	if period >= timeout {
+		t.Errorf("test failed with timeout=%v delay=%v cancel=%v period=%v", timeout, delay, cancel, period)
+	} else {
+		t.Logf("test ok with timeout=%v delay=%v cancel=%v period=%v", timeout, delay, cancel, period)
 	}
 }
 
-func testDelayContext(timeout, delay time.Duration) time.Duration {
+func testDelayContext(timeout, delay, cancel time.Duration) time.Duration {
 	startTm := time.Now()
 	ctx, ctxCancel := context.WithTimeout(context.Background(), timeout)
 	defer ctxCancel()
 	newCtx, newCtxCancel := DelayContext(ctx, delay)
 	defer newCtxCancel()
+	if cancel > 0 {
+		go func() {
+			<-time.After(cancel)
+			newCtxCancel()
+		}()
+	}
 	<-newCtx.Done()
 	return time.Now().Sub(startTm)
 }
