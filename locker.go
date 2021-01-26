b8a07406fe125f0e0988f5f1df003c39dcc97861
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-01-26 11:26:40 +0300

    implemented Locker

---
+++ locker.go
@@ -0,0 +1,61 @@
+package xcontext
+
+import (
+	"context"
+	"sync"
+)
+
+type Locker struct {
+	mu sync.Mutex
+	ch chan struct{}
+}
+
+func (l *Locker) Lock(ctx context.Context) error {
+	l.init()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case l.ch <- struct{}{}:
+			return nil
+		}
+	}
+}
+
+func (l *Locker) Unlock() {
+	l.init()
+	if len(l.ch) <= 0 {
+		panic("unlock of unlocked Locker")
+	}
+	<-l.ch
+}
+
+func (l *Locker) TryLock(ctx context.Context) error {
+	l.init()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case l.ch <- struct{}{}:
+		return nil
+	default:
+		return ErrAlreadyLocked
+	}
+}
+
+func (l *Locker) init() {
+	if l.ch != nil {
+		return
+	}
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if l.ch != nil {
+		return
+	}
+	l.ch = make(chan struct{}, 1)
+}
